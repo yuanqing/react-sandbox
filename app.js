98605e873e5d26a3541d70cc37289bb0825eecd4
yuanqing <desk@yuanqing.sg>
2015-10-12 22:27:00 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+  render: function() {
+    return <div>Hello, {this.props.name}!</div>;
+  }
+});
+
+ReactDOM.render(<Hello name="World" />, document.querySelector('.app'));
