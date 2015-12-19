86637e449325d46c51d14591ba9512c6a9fc9cbc
yuanqing <desk@yuanqing.sg>
2015-12-19 22:47:26 +0800

    Use ES2015 syntax

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-'use strict';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var Hello = React.createClass({
-  render: function() {
+class Hello extends Component {
+  render() {
     return <div>Hello, {this.props.name}!</div>;
   }
-});
+}
 
-ReactDOM.render(<Hello name="World" />, document.querySelector('.app'));
+render(<Hello name="World" />, document.querySelector('.app'));
