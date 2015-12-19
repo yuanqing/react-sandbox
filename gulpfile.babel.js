17440b4cdbef4c3f4b67e3698833af443e0ca9b1
yuanqing <desk@yuanqing.sg>
2015-12-19 22:41:36 +0800

    Upgrade dependencies; cleanup

---
+++ gulpfile.babel.js
@@ -0,0 +1,99 @@
+import opn from 'opn';
+import gulp from 'gulp';
+import http from 'http';
+import nopt from 'nopt';
+import gutil from 'gulp-util';
+import buffer from 'vinyl-buffer';
+import source from 'vinyl-source-stream';
+import babelify from 'babelify';
+import ecstatic from 'ecstatic';
+import watchify from 'watchify';
+import browserify from 'browserify';
+import runSequence from 'run-sequence';
+
+const PORT = 8888;
+const JS_VENDOR_MODULES = ['react', 'react-dom'];
+const JS_APP_FILE = 'app.js';
+const DIST_DIR = 'dist';
+const DIST_VENDOR_FILE = 'vendor.js'
+const DIST_APP_FILE = 'app.js'
+
+const args = nopt({
+  open: String
+}, {
+  o: ['--open', 'google chrome']
+});
+
+const babelifyOptions = {
+  presets: ['es2015', 'react']
+};
+
+function bundleApp(b) {
+  return b.bundle()
+    .pipe(source(DIST_APP_FILE))
+    .pipe(buffer())
+    .pipe(gulp.dest(DIST_DIR));
+};
+
+gulp.task('build', ['build:vendor', 'build:app']);
+
+gulp.task('build:vendor', () => {
+  return browserify()
+    .require(JS_VENDOR_MODULES)
+    .bundle()
+    .pipe(source(DIST_VENDOR_FILE))
+    .pipe(buffer())
+    .pipe(gulp.dest(DIST_DIR));
+});
+
+gulp.task('build:app', () => {
+  const b = browserify({
+    entries: [JS_APP_FILE]
+  });
+  b.external(JS_VENDOR_MODULES)
+  b.transform(babelify, {
+    presets: ['es2015', 'react']
+  })
+  return bundleApp(b);
+});
+
+gulp.task('watch', () => {
+  const b = browserify({
+    entries: [JS_APP_FILE],
+    debug: true,
+    cache: {},
+    packageCache: {}
+  });
+  b.external(JS_VENDOR_MODULES);
+  b.transform(babelify, babelifyOptions);
+  b.plugin(watchify);
+  b.on('error', gutil.log);
+  b.on('log', gutil.log);
+  b.on('update', () => {
+    bundleApp(b);
+  });
+  bundleApp(b);
+});
+
+gulp.task('serve', (callback) => {
+  http.createServer(ecstatic({
+    root: '.'
+  })).listen(PORT, () => {
+    if (args.open) {
+      const url = 'http://localhost:' + PORT;
+      gutil.log(gutil.colors.green('Opening', url));
+      opn(url, {
+        app: args.open,
+        wait: false
+      }, callback);
+    }
+  });
+});
+
+gulp.task('default', () => {
+  runSequence(
+    'build',
+    'watch',
+    'serve'
+  );
+});
