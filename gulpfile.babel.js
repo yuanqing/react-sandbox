7c87690b98b7bee30e1aa92de3a8d4e440eeced9
yuanqing <desk@yuanqing.sg>
2016-01-13 23:00:55 +0800

    Add Sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ import opn from 'opn';
 import gulp from 'gulp';
 import http from 'http';
 import nopt from 'nopt';
+import sass from 'gulp-sass';
 import gutil from 'gulp-util';
 import eslint from 'gulp-eslint';
 import buffer from 'vinyl-buffer';
@@ -11,11 +12,13 @@ import babelify from 'babelify';
 import ecstatic from 'ecstatic';
 import watchify from 'watchify';
 import browserify from 'browserify';
+import sourcemaps from 'gulp-sourcemaps';
 import runSequence from 'run-sequence';
 
 const PORT = 8888;
 const JS_VENDOR_MODULES = ['react', 'react-dom'];
 const JS_APP_FILE = 'app.js';
+const CSS_APP_FILE = 'app.scss';
 const DIST_DIR = 'dist';
 const DIST_VENDOR_FILE = 'vendor.js';
 const DIST_APP_FILE = 'app.js';
@@ -37,20 +40,19 @@ function bundleApp(b) {
     .pipe(gulp.dest(DIST_DIR));
 };
 
-gulp.task('clean', () => {
-  return del(DIST_DIR);
-});
-
-gulp.task('lint', () => {
+gulp.task('js:lint', () => {
   return gulp.src(['app.js', __filename])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('build', ['build:vendor', 'build:app']);
+gulp.task('js:build', [
+  'js:build:vendor',
+  'js:build:app'
+]);
 
-gulp.task('build:vendor', () => {
+gulp.task('js:build:vendor', () => {
   return browserify()
     .require(JS_VENDOR_MODULES)
     .bundle()
@@ -59,7 +61,7 @@ gulp.task('build:vendor', () => {
     .pipe(gulp.dest(DIST_DIR));
 });
 
-gulp.task('build:app', () => {
+gulp.task('js:build:app', () => {
   const b = browserify({
     entries: [JS_APP_FILE]
   });
@@ -70,7 +72,7 @@ gulp.task('build:app', () => {
   return bundleApp(b);
 });
 
-gulp.task('watch', () => {
+gulp.task('js:watch', () => {
   const b = browserify({
     entries: [JS_APP_FILE],
     debug: true,
@@ -87,6 +89,36 @@ gulp.task('watch', () => {
   });
   bundleApp(b);
 });
+
+gulp.task('css:build', () => {
+  return gulp.src(CSS_APP_FILE)
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DIST_DIR));
+});
+
+gulp.task('css:watch', () => {
+  gulp.watch(CSS_APP_FILE, ['css:build']);
+});
+
+gulp.task('clean', () => {
+  return del(DIST_DIR);
+});
+
+gulp.task('lint', [
+  'js:lint',
+]);
+
+gulp.task('build', [
+  'js:build',
+  'css:build'
+]);
+
+gulp.task('watch', [
+  'js:watch',
+  'css:watch'
+]);
 
 gulp.task('serve', (callback) => {
   http.createServer(ecstatic({
